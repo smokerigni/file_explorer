df22432b31675b73b9e81dc22806ab3be27d0b84
smokerigni <smokerigni@gmail.com>
2016-12-11 17:08:11 +0100

    main js renders index html

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-//Main file
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var mainWindow = null;
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+app.on('ready', function () {
+    mainWindow = new BrowserWindow({width: 900, height: 600});
+
+    mainWindow.loadURL('file://' + __dirname + '/views/index.html');
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+});
 
